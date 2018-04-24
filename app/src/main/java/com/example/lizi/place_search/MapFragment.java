99011ea9115d60e059960ce2dc71a615e17f5ca6
Zack <ziliusc666@gmail.com>
2018-04-24 12:16:25 -0700

    added autocomplete and spinner to map tab with no listener on them

--- app/src/main/java/com/example/lizi/place_search/MapFragment.java
+++ app/src/main/java/com/example/lizi/place_search/MapFragment.java
@@ -8,21 +8,34 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback{
-    final static String TAG = "map";
+    private final static String TAG = "map";
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136)
+    );
 
 
     private LatLng mLatLng;
     private String placeName;
 
+    private Spinner travelModesSpinner;
+    private AutoCompleteTextView addressAutocomplete;
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    protected GeoDataClient mGeoDataClient;
 
 
     public MapFragment() {
@@ -47,6 +60,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         Log.d(TAG, "place name: " + placeName);
         Log.d(TAG, "OnCreateView  LatLng:" + mLatLng.toString());
 
+        travelModesSpinner = rootView.findViewById(R.id.travel_modes_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.travel_mode_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        travelModesSpinner.setAdapter(adapter);
+
+        mGeoDataClient = Places.getGeoDataClient(getActivity());
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(getActivity(), mGeoDataClient, LAT_LNG_BOUNDS, null);
+        addressAutocomplete = rootView.findViewById(R.id.map_input_address);
+        addressAutocomplete.setAdapter(mPlaceAutocompleteAdapter);
+
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         return rootView;
@@ -57,6 +83,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         googleMap.addMarker(new MarkerOptions().position(mLatLng).title(placeName));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(mLatLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(13));
 
     }
 }
